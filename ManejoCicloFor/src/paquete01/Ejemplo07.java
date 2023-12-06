75681d2ec336bf7e2b277d7922140d945a5ab272
Cody <codycabreraa@gmail.com>
2023-12-06 16:13:57 -0500

    Cambios

--- ManejoCicloFor/src/paquete01/Ejemplo07.java
+++ ManejoCicloFor/src/paquete01/Ejemplo07.java
@@ -15,15 +15,17 @@ public class Ejemplo07 {
         String mensajeFinal = "";
         for (int i = 1; i <= 10; i++) { // for 01
             // System.out.printf("Tabla de multiplicar del número %d\n", i);
-            mensajeFinal = String.format("%sTabla de multiplicar del número %d\n", 
-                    mensajeFinal, i);
+            mensajeFinal = String.format("%sTabla de multiplicar del número %d\n", mensajeFinal, i);
             for (int contador = 1; contador <= 12; contador++) {
                 operacion = i * contador;
                 // System.out.printf("%d x %d = %d\n", i, contador, operacion);
+                mensajeFinal = String.format("%s%d x %d = %d\n", mensajeFinal, i, contador, operacion);
             }
             // System.out.println("\n");
+            mensajeFinal = String.format("%s\n", mensajeFinal);
         } // termina for 01
         
         System.out.printf("%s\n", mensajeFinal);
+        // tenemos un ciclo dentro de otro ciclo donde la variable i es el numero de la tabla y la variable contador solo sera el valor que nos ayude con cada operaciuon
     }
 }
